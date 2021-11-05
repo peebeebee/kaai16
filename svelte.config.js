56c4e80a163e34ed9da1aa995cd579a04b54881a
Peter Briers <peter.briers@gmail.com>
2021-11-05 12:35:57 +0100

    Add svelte-image lib

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
+import image from "svelte-image";
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		...image(),
+	}),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
